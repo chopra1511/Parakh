1ec31d40cc81d698e8f73b5e6928303018ea5d6e
Rahul Chopra <63909178+chopra1511@users.noreply.github.com>
2024-07-01 07:43:16 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ store.on("error", function (error) {
 
 app.use(
   cors({
-    origin: "https://parakh-theta.vercel.app",
+    origin: "https://parakh-theta.vercel.app/",
     credentials: true,
   })
 );
